cbc23d2a1708592f9934df64bd70d84abd326b0a
vs74 <vasudevsharma74@yahoo.com>
2020-03-27 13:22:41 +0000

    Add Files

---
+++ install_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git checkout CNN
+pip install virtualenv
+virtualenv eeg_test --python=3.6
+source eeg_test/bin/activate
+pip install -r requirements.txt
+mkdir input
+cd input/ 
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Ec4P523hO2bSWKqc2YaEVSheZXnU6Kc4' -O 1filtered.mat
+cd ../src/
