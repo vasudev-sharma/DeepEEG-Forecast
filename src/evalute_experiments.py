1b8bcaf12bb7343fbc5319992e6aaa7e8c544757
vs74 <vasudevsharma74@yahoo.com>
2020-05-05 19:33:25 +0000

    Add avg experiment and correct evalulate_experiment.py

--- src/evalute_experiments.py
+++ src/evalute_experiments.py
@@ -4,18 +4,25 @@ import numpy as np
 with open("experiment_log.json", "r") as read_file:
         data = read_file.readlines() 
         avg_corr = []
-        print(data)
+        #print(data)
         for i in range(len(data)):
-            print(data[i])
+            #print(data[i])
             avg_corr.append(json.loads(data[i])["Experiment_"+str(i+1)])
-        print(avg_corr)
+        #print(avg_corr)
 
 
-print(np.array(avg_corr).mean())
+#print(np.array(avg_corr).mean(axis = 0))
+
+
 
 with open("models.json", "a") as write_file:
-        json.dump(np.array(avg_corr).mean(), write_file)
+        data = np.array(avg_corr).mean(axis = 0)
+        print(data)
+        print(data.shape)
+        json.dump(data.tolist(), write_file)
         write_file.write("\n")
+
+
 
 
 with open("experiment_log.json", "w") as write_file:
