1abd7fe1ac8ca628fceee5a3000c112b3d9336c7
vs74 <vasudevsharma74@yahoo.com>
2020-05-07 21:43:29 +0000

    Update utils

--- src/utils.py
+++ src/utils.py
@@ -64,22 +64,24 @@ def show_data(data,channel, trial):
 
 
 
-def sanity_check(pred, true):
+def sanity_check(true, pred):
 
     if len(pred.shape) == 2:
         return
     
     else:
 
-        plt.plot(true[1550,:,0],'r')
-        plt.plot(pred[1550,:,0],'b--')
+        plt.plot(true[1550,:,0],'r', label = "True value")
+        plt.plot(pred[1550,:,0],'b--', label = "Predicted value")
+        plt.legend()
         plt.savefig("../images/sanity_check_prediction_horizon.png")
 
         plt.figure()
 
         #sanity check: the "sequence" is present both in the first AND in the second dimension
-        plt.plot(true[:500,0,0],'r')
-        plt.plot(pred[:500,0,0],'b--')
+        plt.plot(true[:500,0,0],'r', label = "True value")
+        plt.plot(pred[:500,0,0],'b--', label = "Predicted value")
+        plt.legend()
         plt.savefig("../images/sanity_check_prediction_batch.png")
         #not so true when z_score_outputs is true, because the 2nd dimension was z-scored, the first wasn't
         #and, not so true with fake_data, i.e. when the signal is simple. The 'cheat' is only used for real EEG data.
