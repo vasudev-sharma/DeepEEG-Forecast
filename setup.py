8e374610c790365abd7d5e82b6a3dd24b43375ac
vs74 <vasudevsharma74@yahoo.com>
2020-12-25 23:43:18 +0530

    Add Setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+
+
+setup(
+    name = "EEG",
+    version = "0.0.1",
+    author = "Vasudev Sharma",
+    author_email = "vasudevsharma74@yahoo.com",
+    description = ("Deep Learning Toolbox for EEG Forecasting"),
+    license = "MIT",
+    keywords = "Deep Learning Forecasting EEG",
+    url = "http://packages.python.org/an_example_pypi_project",
+    #packages=['an_example_pypi_project', 'tests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
