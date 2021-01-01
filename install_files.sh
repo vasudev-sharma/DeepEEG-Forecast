37bfad29e166dedf12b197bc026b9360718830a1
vs74 <vasudevsharma74@yahoo.com>
2021-01-01 11:53:27 +0000

    remove directory already exists error and activate virtual environment error

--- install_files.sh
+++ install_files.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 pip install virtualenv
 virtualenv eeg_env --python=3.6
-source eeg_env/bin/activate
+/bin/bash -c "eeg_env/bin/activate"
 pip install -r requirements.txt
-mkdir input
-mkdir images
+mkdir -p input
+mkdir -p images
 cd input/ 
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lA9mQ-7jbSP96gsdWV5eSWxyCL2_Ov52' -O 1filtered.mat
 cd ../src/
-mkdir experiment
-wandb login
-
+mkdir -p experiment 
